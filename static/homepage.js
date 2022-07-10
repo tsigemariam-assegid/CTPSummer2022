6877b12f4ab49fead5680fed3a97ef567f27dab8
unknown <abitad2015@gmail.com>
2022-07-11 02:30:33 +0300

    added homepage

---
+++ static/homepage.js
@@ -0,0 +1,12 @@
+
+function beg_func() {
+    window.location.href = "https://www.example.com";
+}
+
+function inter_func() {
+    window.location.href = "https://www.example.com";
+}
+
+function adv_func() {
+    window.location.href = "https://www.example.com";
+}
